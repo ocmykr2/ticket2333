222ac9840550df949f565e3daa410fd75c4fca9f
ocmykr2 <84970740+ocmykr2@users.noreply.github.com>
2021-06-10 22:47:22 +0800

    Add files via upload

---
+++ FileOperator.hpp
@@ -0,0 +1,36 @@
+#ifndef FileOperatorr
+#define FileOperatorr
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+class Fileoperator {
+    public:
+    void NewFile(char *FileName) {
+        std::ofstream who(FileName);
+        who.close();
+    }
+
+    template <typename T>
+    void get(std::fstream &curFile, int pos, int num, T *where) const {
+        curFile.clear(); // Çå³ý´íÎó×´Ì¬
+		curFile.seekg(pos);
+        curFile.clear();
+		curFile.read( reinterpret_cast<char*>(where), int(num * sizeof(T)) );
+    }
+
+    int getend(std::fstream &curFile) const {
+        curFile.clear();
+		curFile.seekg(0, std::ios::end);
+		return (int)curFile.tellg();
+    }
+
+    template <typename T>
+    void write(std::fstream &curFile, int pos, int num, T *where) const {
+        curFile.clear();
+        if(pos == -1) pos = getend( curFile );
+        curFile.seekg( pos );
+        curFile.write( reinterpret_cast<char*>(where),  int(num * sizeof(T)) );
+    }
+}FileOperator;
+#endif
